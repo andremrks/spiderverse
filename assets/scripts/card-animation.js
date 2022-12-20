7a9e18b32ca2785ff1c2d758f2d6f2dd94244d42
André Marques <andrelcm89pt@gmail.com>
2022-12-20 18:05:13 +0000

    finishing page

--- assets/scripts/card-animation.js
+++ assets/scripts/card-animation.js
@@ -19,3 +19,19 @@ function addEventListennerToCards () {
 }
 
 document.addEventListener("DOMContentLoaded", addEventListennerToCards)
+
+function selectCarouselItem(selectedBtnElement) {
+    const selectedItem = selectedBtnElement.id
+    const carousel = document.querySelector('.s-cards-carousel')
+    const transform = carousel.style.transform
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i) //using Regex
+    const rotateYDeg = -90 * (Number(selectedItem) - 1) // 90 cause we have 4 card items (360deg / 4(+-)) / "-1" to my start item keeps centralized
+    const newTrasform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`)
+
+    carousel.style.transform = newTrasform
+
+    const activeButton = document.querySelector('.s-controller__button--active')
+    activeButton.classList.remove('s-controller__button--active')
+    selectedBtnElement.classList.add('s-controller__button--active')
+    
+}
